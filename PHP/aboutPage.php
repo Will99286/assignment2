055d55be7cfa757e9f177ef510016189cfae9145
Will99286 <44880449+Will99286@users.noreply.github.com>
2020-04-10 16:06:29 -0600

    about page uploaded

---
+++ PHP/aboutPage.php
@@ -0,0 +1,33 @@
+<?php
+    include 'header.php';
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/aboutus.css">
+</head>
+<body>
+    <div class="fisrtClass">
+    <h1>About Page</h1>
+    </div>
+    <div id="firstSection">
+        <h2>Brief Description</h2>
+        <p>Mount Royal University | COMP3512 | WEB 2 | Randy Connolly | Winter 2020<br>
+        Skill used: HTML | CSS | Javascript | PHP<br>
+        github repo:<a href="https://github.com/Will99286/assignment2">https://github.com/Will99286/assignment2</a>
+    </p>
+    </div>
+<div class="secondClass"></div>
+   <div id="secSection">
+   <h2>Group Info</h2>
+   <ul id="whoweare">
+        <li>Will Ji <a href="https://github.com/"></a> </li> 
+        <li>name <a href="https://github.com/"></a> </li>    
+        <li>name<a href="https://github.com/"></a> </li> 
+        <li>name<a href="https://github.com/"></a> </li> 
+   </ul>
+</div>
+   
+</body>
+</html>
