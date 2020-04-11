ac9f980f9b967eb8192d8e8b37a1fb72669b2b8d
imark776 <62232769+imark776@users.noreply.github.com>
2020-04-11 08:04:33 -0600

    Add files via upload

--- PHP/aboutPage.php
+++ PHP/aboutPage.php
@@ -5,27 +5,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../CSS/aboutus.css">
+    <link rel="stylesheet" href="../CSS/aboutPage.css">
 </head>
 <body>
-    <div class="fisrtClass">
-    <h1>About Page</h1>
-    </div>
-    <div id="firstSection">
+    <h1>About</h1>
+    <div id="section">
         <h2>Brief Description</h2>
-        <p>Mount Royal University | COMP3512 | WEB 2 | Randy Connolly | Winter 2020<br>
-        Skill used: HTML | CSS | Javascript | PHP<br>
-        github repo:<a href="https://github.com/Will99286/assignment2">https://github.com/Will99286/assignment2</a>
-    </p>
-    </div>
-<div class="secondClass"></div>
-   <div id="secSection">
-   <h2>Group Info</h2>
+        Mount Royal University | COMP3512 | WEB 2 | Randy Connolly | Winter 2020<br />
+        <strong>Skills Used:</strong> HTML | CSS | Javascript | PHP<br />
+        <strong>GitHub Repo:</strong> <a href="https://github.com/Will99286/assignment2">https://github.com/Will99286/assignment2</a>
+    
+   <br /><h2>Group Info</h2>
    <ul id="whoweare">
         <li>Will Ji <a href="https://github.com/"></a> </li> 
-        <li>name <a href="https://github.com/"></a> </li>    
-        <li>name<a href="https://github.com/"></a> </li> 
-        <li>name<a href="https://github.com/"></a> </li> 
+        <li>Ivan Markovic <a href="https://github.com/"></a> </li>    
+        <li>Joseph Oliva<a href="https://github.com/"></a> </li> 
+        <li>Arnold<a href="https://github.com/"></a> </li> 
    </ul>
 </div>
    
