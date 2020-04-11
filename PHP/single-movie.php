ce0b682da4af41aca4d17874dcc89625f5912a2f
imark776 <62232769+imark776@users.noreply.github.com>
2020-04-11 14:33:01 -0600

    Add files via upload

--- PHP/single-movie.php
+++ PHP/single-movie.php
@@ -72,7 +72,7 @@ include 'header.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../CSS/singlemovie.css">    
+    <link rel="stylesheet" href="../CSS/single-movie.css">    
 </head>
 
 <body>
