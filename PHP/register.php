9e62fb858ce12d55542d53f0e7fe211ecb6fabfe
Will99286 <44880449+Will99286@users.noreply.github.com>
2020-04-12 00:18:06 -0600

    Update register.php

--- PHP/register.php
+++ PHP/register.php
@@ -54,8 +54,6 @@ function checkEmail ($email) {
     $connection = setConnectionInfo(DBCONNSTRING,DBUSER,DBPASS);
     $sql = 'SELECT email FROM d1eamej0bobjmtrf.user';
     $statement = runQuery($connection, $sql, null);
-    echo "hello";
-    echo $statement;
     foreach ($statement as $s){
       if ($email == $s[0]){
           $result = strcmp($email, $s[0]);
